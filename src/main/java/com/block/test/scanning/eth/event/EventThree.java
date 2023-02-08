e0890fb0b78e0381dfa40db8ba9677334f9fef47
Beerus <yuyemail@163.com>
2023-02-08 17:16:24 +0800

    Add the ability to scan tron chains

--- src/main/java/com/block/test/scanning/eth/event/EventThree.java
+++ src/main/java/com/block/test/scanning/eth/event/EventThree.java
@@ -16,15 +16,19 @@ public class EventThree implements EthMonitorEvent {
      */
     @Override
     public EthMonitorFilter ethMonitorFilter() {
-//        return EthMonitorFilter.builder()
-//                .setToAddress("0x552115849813d334C58f2757037F68E2963C4c5e");
-
-        return null;
+        return EthMonitorFilter.builder()
+                .setToAddress("0x552115849813d334C58f2757037F68E2963C4c5e");
     }
 
     @Override
     public void call(TransactionModel transactionModel) {
+
+        // 符合条件的交易记录
         EthBlock.TransactionObject transactionObject = transactionModel.getEthTransactionModel().getTransactionObject();
+
+        // 本条交易记录所在的块信息
+        EthBlock ethBlock = transactionModel.getEthTransactionModel().getEthBlock();
+
 
         String template = "EventThree 扫描到了, hash:{0}, hash:{1}, from:{2}, to: {3}, input: {4}";
         template = template.replace("{0}", transactionModel.getEthTransactionModel().getEthBlock().getBlock().getHash());
